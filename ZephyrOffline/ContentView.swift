cc4ebef1dd970434f74b9fb231e2288848ad1790
wgaliano <wgaliano22@fed.idserve.net>
2023-11-04 11:53:49 +0100

    Initial Commit

---
+++ ZephyrOffline/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZephyrOffline
+//
+//  Created by Walter Galiano on 04/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZephyrOffline/ZephyrOfflineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZephyrOfflineApp.swift
+//  ZephyrOffline
+//
+//  Created by Walter Galiano on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ZephyrOfflineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
